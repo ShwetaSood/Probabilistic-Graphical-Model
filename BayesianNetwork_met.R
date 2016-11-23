6206e95dd2f1bded067422016c98b0c3e08f1840
Shweta Sood <shweta12164@iiitd.ac.in>
2016-11-24 00:37:16 +0530

    minor change

--- BayesianNetwork_met.R
+++ BayesianNetwork_met.R
@@ -12,11 +12,6 @@ rm(bayesian_data_temp)
 
 #train test data
 
-# p = 70 # percentage of data to be kept in train
-# index = sample((1:nrow(bayesian_data)),p/100*nrow(bayesian_data),replace = FALSE)
-# train_bayesian_data = bayesian_data[index,]
-# test_bayesian_data = bayesian_data[-index,]
-
 # Network Building Process
 train = bayesian_data
 rm(bayesian_data)
@@ -31,9 +26,7 @@ node_names = names(train)
 print("Learning structure")
 
 #Score Based Structure Learning
-#score = c("bde","aic","bic","k2")
 sc = "bde"
-#-------check# setwd("GAF_final_models_data/")
 #different ways of structural learning
 t0 <-Sys.time()
 #net_tabu_optimised = tabu(train, whitelist=wl, blacklist=bl, score=sc, optimized = TRUE, debug = TRUE)
@@ -52,16 +45,10 @@ model = net_tabu
 #model_optimised = net_tabu_optimised 
 
 class(net_tabu)
-#class(net_tabu_optimised)
-#plot(net_tabu,type="lines")
 
 # #markov balnket
 data_markov_b = as.data.frame(model$arcs) # graph for markov blanket 
 save(data_markov_b,file=paste("data_markov_b_",str_split_fixed(toString(Sys.time()), " ", 2)[ ,1],".Rdata",sep=""))
-
-# data_markov_b_op = as.data.frame(model_optimised$arcs) # graph for markov blanket 
-# save(data_markov_b_op,file=paste("data_markov_b_op_",Sys.time(),".Rdata",sep=""))
-
 
 #data_markov_b_h = as.data.frame(model_hc$arcs)
 
@@ -74,26 +61,4 @@ plot_markov_blanket(data_markov_b[markov_blanket(data_markov_b,node)$index,],nod
 cpt_train = bn.fit(model,train,method="bayes", iss=10)
 save(cpt_train,file=paste("cpt_train_",str_split_fixed(toString(Sys.time()), " ", 2)[ ,1],".Rdata",sep=""))
 
-# cpt_train_optimised = bn.fit(model_optimised,train,method="bayes", iss=10)
-# save(cpt_train_optimised,file=paste("cpt_train_optimised_",Sys.time(),".Rdata",sep=""))
-
-# #cpt_train_hc = bn.fit(model_hc,train,method="bayes", iss=10)
 write.dsc(cpt_train,file=paste("MA_Ns_2014_conv",".dsc",sep=""))
-#write.dsc(cpt_train_optimised,file=paste("GAF_FINAL_optimised_removing_irrelevant_similar_",Sys.time(),".dsc",sep=""))
-
-# # 
-# out_new = c()
-# j = 1
-# #validation_bayes_data_overall = as.data.frame(lapply(validation_bayes_data_overall, factor))
-# inference_data = train
-# node = "part_perc_bin"
-# for(i in 1:nrow(inference_data)){
-#   #pv = inference_data[i,""]
-#   #evidence = as.list(pv[1,setdiff(colnames(pv), node)])
-#   prob = cpquery(cpt_train, event = (part_per == c(1,2,3)), evidence = (COMPANY_STATE == "AL"), method="lw")
-#   out_new = c(out_new,prob)
-#   if(i > 1000*j){print(i); j = j+1}
-#   print(i)
-# }
-# 
-# predict = out
